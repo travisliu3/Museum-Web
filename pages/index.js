334e5c500214ea3cf94386b3c1673a9e296a0712
travisliu3 <100375067+travisliu3@users.noreply.github.com>
2024-04-01 11:49:41 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,5 @@
 /*********************************************************************************
-*  WEB422 – Assignment 5
-*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
-*  No part of this assignment has been copied manually or electronically from any other source
-*  (including web sites) or distributed to other students.
-* 
-*  Name: ___Travis Liu___ Student ID: ___156740201___ Date: ___3 April, 2023___
-*
 *  Vercel App (Deployed) Link: ___https://web422assignment6-snowy.vercel.app/________
-*
 ********************************************************************************/
 
 import Image from 'next/image'
